f83d16f5974bf1433953dacc893bc91d35f26d1e
fannom466 <gujnghj@mail.ru>
2022-06-12 17:10:49 +0300

    debug

--- src/App.js
+++ src/App.js
@@ -3,32 +3,26 @@ import Header from './components/Header'
 import Categories from './components/Categories'
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
+import pizzas from './assets/pizzas.json';
+
 
 function App() {
   return (
-    <div className="App">
-    <div class="wrapper">
+    <div className='App'>
+    <div className='wrapper'>
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className='content'>
+        <div className='container'>
+          <div className='content__top'>
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-          <PizzaBlock title='Компаньола' price={960}/>
-          <PizzaBlock title='карбонара' price={400}/>
-          <PizzaBlock title='мексиканская' price={567}/>
-          <PizzaBlock title='острая с грибами' price={950}/>
-          <PizzaBlock title='4 сезона' price={234}/>
-          <PizzaBlock title='вегатерианская' price={568}/>
-          <PizzaBlock title='детская' price={843}/>
-          <PizzaBlock title='итальянская' price={560}/>
-          <PizzaBlock title='Крудо' price={798}/>
-          <PizzaBlock title='4 сыра' price={852}/>
-          <PizzaBlock title='Маринара' price={234}/>
-          <PizzaBlock title='Боскайола' price={906}/>
+          <h2 className='content__title'>Все пиццы</h2>
+          <div className='content__items'>
+         {
+          pizzas.map(obj =>  <PizzaBlock title={obj.title} price={obj.price} imageUrl={obj.imageUrl}/>)
+         }
+         
           </div>
         </div>
       </div>
